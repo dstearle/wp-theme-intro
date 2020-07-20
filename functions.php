d424e4818b5fc5a04c70cc13981c21c183a3350a
dstearle <dallas.st.earle@gmail.com>
2020-07-19 17:21:12 -0700

    Began integrating blog posts into theme

--- functions.php
+++ functions.php
@@ -3,9 +3,19 @@
 	// Register Nav Walker class_alias
 	require get_template_directory() . '/bootstrap-navwalker.php';
 
-	// Regist Nav Menus
+	// Register Nav Menus
 	register_nav_menus( array(
 		'menu-1' => esc_html__( 'Primary', 'theme-textdomain' ),
 	) );
+
+	// Excerpt Length Control
+	function set_excerpt_length() {
+
+		// The length of the post excerpt to be used
+		return 45;
+
+	}
+
+	add_filter('excerpt_length', 'set_excerpt_length');
 
 ?>
